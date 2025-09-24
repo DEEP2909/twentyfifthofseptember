6f623782d7552eae44bca2f2828e9bd8da619155
DEEP2909 <155870496+DEEP2909@users.noreply.github.com>
2025-09-24 20:43:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,16 +6,4 @@ export default defineConfig({
   base: '/twentyfifthofseptember/', // 👈 must match your repo name
 })
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+
